30fd2bb34b9cbcb60baa15be64d6784ab60b0f0d
井筒 将之 <izutsushono.0415122@gmail.com>
2026-01-01 11:25:59 +0900

    キャッシュ削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-title" content="料理メモ" />
       </head>
       <body>{children}</body>
     </html>
